dcea794a6a2612b55e04f494330c5c993a264774
vjbhawani <choudharybsc@gmail.com>
2019-09-27 21:47:49 +0530

    meaning of Knashack problem

--- src/main/java/algorithm/dynamicporgramming/Knapsack0_1.java
+++ src/main/java/algorithm/dynamicporgramming/Knapsack0_1.java
@@ -2,10 +2,13 @@ package algorithm.dynamicporgramming;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
 
 /**
  * 0/1 means either you pick once or not-pick at all
+ * it's called knapsack because knapsack means backpack, so if you have a backpack (means a finite space) which items
+ * will you pick so that the total-value of backpack is maximum
+ *
+ * doubt: what will happen when the input-maxWeight is too high, there will be too much empty iterations
  */
 public class Knapsack0_1 {
     public static void main(String[] args) {
@@ -37,10 +40,8 @@ public class Knapsack0_1 {
                         maxValueIfThisWeighNotSelected = matrix[i-1][j];
                     }
                     matrix[i][j] = Math.max(value+leftValue, maxValueIfThisWeighNotSelected);
-                } else {
-                    if(i != 0) {
-                        matrix[i][j] = matrix[i-1][j];
-                    }
+                } else if (i != 0) {
+                    matrix[i][j] = matrix[i - 1][j];
                 }
                 System.out.print(matrix[i][j] + " ,");
             }
@@ -67,12 +68,13 @@ public class Knapsack0_1 {
 //            }
 //        }
 
-        backTrackResultWeight(matrix, row, col, weights, resultWeights);
-        resultWeights.stream().forEach(System.out::println);
+        backTrackResultWeight(matrix, row, col, weights).forEach(System.out::println);
+//        resultWeights.stream().forEach(System.out::println);
     }
 
-    private static void backTrackResultWeight(int[][] matrix, int row, int col, int[] weights, List<Integer> resultWeights) {
+    private static List<Integer> backTrackResultWeight(int[][] matrix, int row, int col, int[] weights) {
         if(row != 0 && col != 0) {
+            List<Integer> resultWeights = new ArrayList<>();
             int val = matrix[row][col];
             int topVal = 0;
             if (row != 0) {
@@ -85,8 +87,10 @@ public class Knapsack0_1 {
                 row--;
                 col -= weights[row];
             }
-            backTrackResultWeight(matrix, row, col, weights, resultWeights);
+            resultWeights.addAll(backTrackResultWeight(matrix, row, col, weights));
+            return resultWeights;
         }
+        return new ArrayList<>();
     }
 
 }
