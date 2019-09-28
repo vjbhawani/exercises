afa23028748ff273b7fb4db6c42ffab78f6b42f7
vjbhawani <choudharybsc@gmail.com>
2019-09-28 22:22:48 +0530

    fly weight

--- src/main/java/designpatterns/structural/Flyweight.java
+++
@@ -1,4 +0,0 @@
-package designpatterns.structural;
-
-public class Flyweight {
-}
---
+++ src/main/java/designpatterns/structural/flyweight/Flyweight.java
@@ -0,0 +1,9 @@
+package designpatterns.structural.flyweight;
+
+/**
+ * flyweight derives it's name from boxing category flyweight, which is light-weight category.
+ *  note: patterns wise it means sharing light-weight/fine-grained objects, to reduce the space.
+ *  fine-grained objects can be cached, instead of generating new objects
+ */
+public class Flyweight {
+}
